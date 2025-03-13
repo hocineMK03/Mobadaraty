f2d102062531740e2561039332b44a6fc29d9884
hocineMK03 <hocine182003@gmail.com>
2025-03-13 12:48:30 +0100

    add donations
    added donations functionality for volunteers

--- src/app.js
+++ src/app.js
@@ -1,25 +1,29 @@
 const express = require("express");
-
 const { connectDB } = require("./config/db");
+const errorHandler = require("./middlewares/errorHandler");
+const v1Routes = require("./routes/v1");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require('cors');
 
 const app = express();
 
-const errorHandler = require("./middlewares/errorHandler");
+// CORS setup first
+app.use(cors());
+app.options('*', cors()); // Enable pre-flight for all routes
 
-const v1Routes = require("./routes/v1");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser")
-
-const cors = require('cors');
-app.use(cors());;
+// Body parsers next
 app.use(express.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Route middleware
 app.use("/api/v1", v1Routes);
 
+// Connect to database
 connectDB();
 
+// Error handler should be last
 app.use(errorHandler);
 
 module.exports = app;
