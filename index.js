b9331b089364fc3840bca5b0a22418a2cbe65141
hocineMK03 <hocine182003@gmail.com>
2025-03-13 12:41:09 +0100

    add donations
    added donations functionality for volunteers

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = require("./src/app");
 const config = require("./src/config/config");
 const port = config.port;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is listening on port ${port}`);
 });
